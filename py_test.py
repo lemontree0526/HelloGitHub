7ad2d706e658e5870298777c2556c729ea58c7aa
zhingheng Ma <zhingheng.ma@zhinghengs-MacBook-Pro.local>
2022-03-18 12:10:35 +0800

    add a pyfile

---
+++ py_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+# 例1：if 基本用法
+ 
+flag = False
+name = 'luren'
+if name == 'python':         
+    flag = True             
+    print 'welcome boss' 
+else:
+    print name 
